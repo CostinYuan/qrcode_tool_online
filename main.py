c083de3e6a247b6dbc031a6050996dc08d2d99ac
jongkhurun <yuanjongkhurun@outlook.com>
2019-07-02 10:44:45 +0800

    实现同一页面显示二维码

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
-from flask import Flask, request, render_template, url_for
+from flask import Flask, request
+import flask
 import qrcode
+import io
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def qr():
-    return render_template('qrcode.html')
+    return flask.render_template('qrcode.html')
 
-@app.route('/qr_img', methods=['POST'])
+@app.route('/qr')
 def qr_img():
     data = request.form.get("data")
+
     img = qrcode.make(data)
-    img.save('static\\{}.png'.format(data))
-    return '<p>快扫我：<img src="%s" alt=""></p>' % url_for('static', filename='{}.png'.format(data))
+    bi = io.BytesIO()  # 创建一个BytesIO对象，用于存储二维码图像数据
+    img.save(bi, "png")  # 调用img对象的save方法将二维码图像数据以PNG编码格式写入bi对象管理的内存空间
+    bi.seek(0)  # 将bi对象内部的位置指针移动到图像数据的其实位置
+
+    return flask.send_file(bi, "image/png")
 
 
 
